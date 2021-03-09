9cfe0522372184b2fa3282636007928c88b1a2e8
Novak Fenocchio <fenomartin6@gmail.com>
2021-03-09 18:19:24 -0300

    quotes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './assets/css/index.css';
 
 import Pomodoro from './components/pomodoro';
+import Quotes from './components/quotes';
 
 function App() {
   return (
     <div className="App">
       <div className='container-sections'>
         <Pomodoro />
+        <Quotes/>
       </div>
     </div>
   );
---
+++ src/components/quotes.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+import quote from '../assets/img/quoteSimbol.png';
+
+export default class Quotes extends Component {
+
+    constructor()
+    {
+        super();
+        this.state = {
+            quote: false
+        }
+    }
+
+    getQuote = async() =>
+    {  
+        const quoteGived = await fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+        .then(res => res.json());
+
+        this.setState({
+            quote: quoteGived.quoteText
+        })
+    }
+
+    
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='container-quotes'>
+                    <div className="quote-container">
+                        <div className="container-quote-image">
+                            <img src={quote} alt=""/> 
+                        </div>
+                        <div className="container-text-quote">
+                        {
+                        this.state.quote == false &&
+                        <div className="container-button">
+                            <button className='btn btn-black btn-quote' onClick={this.getQuote}>Nueva frase</button>
+                        </div>
+                         }
+                         {this.state.quote != false &&
+                         <React.Fragment>
+                            <h1 className='text-qoute'><span>{this.state.quote}</span></h1>
+                         </React.Fragment>
+                        }
+                        </div>
+                        <div className="container-quote-image container-button lastImagequote">
+                            <button className='btn-white btn-next-quote' onClick={this.getQuote}>Nueva frase</button>
+                            <img src={quote}/>
+                        </div>
+                    </div>
+                    
+
+                </div>
+            </React.Fragment>
+        )
+    }
+}
